1543109881e925fec42ac5390dbd72d5ecd848ca
Ilia Idakiev <ilia.idakiev@gmail.com>
2018-11-03 08:55:22 +0200

    quick refactoring

--- week5/lecture/src/index.ts
+++ week5/lecture/src/index.ts
@@ -1,46 +1,65 @@
 import * as http from 'http';
 import * as fs from 'fs';
 import * as path from 'path';
+import { IRouter } from './interfaces/router';
 
-const router: { [key: string]: (req: any, res: any) => void } = {
-  '/': (req: any, res: any) => {
-    const [, , query] = /(.*)\?(.*)/.exec(decodeURIComponent(req.url));
-    const [, filePath] = /filePath=\"(.*)\"/.exec(query);
-    const dataPath = path.join(__dirname, filePath);
+function parsePath(filePath: string = '') {
+  return filePath.replace(/'|"/g, '');
+}
 
-    fs.readFile(dataPath, function (err, content) {
+function missingPathHandler(res: http.ServerResponse): void {
+  res.writeHead(400, { 'Content-type': 'text/plain' });
+  res.write('400 Bad request! Missing file path!');
+  res.end();
+}
+
+const router: IRouter = {
+  '/': (reqQuery, req, res) => {
+    let filePath: string = parsePath(reqQuery['filePath']);
+    if (filePath === '') { missingPathHandler(res); return; }
+    filePath = path.join(__dirname, filePath);
+    fs.readFile(filePath, function (err, content) {
       if (err) {
-        res.writeHead(500, { 'Content-type': 'text' });
+        res.writeHead(500, { 'Content-type': 'text/plain' });
         res.write(err.message);
         res.end();
         return;
       }
-      res.writeHead(200, { 'Content-type': 'text' });
+      res.writeHead(200, { 'Content-type': 'text/plain' });
       res.write(content);
       res.end();
     });
   },
-  // '/stream': (req: any, res: any) => {
-  //   // const readStream = fs.createReadStream(dataPath);
-  //   // readStream.pipe(res);
-  // },
-  '/aa': (req: any, res: any) => {
-    res.writeHead(200, { 'Content-type': 'text' });
+  '/stream': (reqQuery, req: any, res: any) => {
+    let filePath: string = parsePath(reqQuery['filePath']);
+    if (filePath === '') { missingPathHandler(res); return; }
+    filePath = path.join(__dirname, filePath);
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(res);
+  },
+  '/aa': (reqQuery, req: any, res: any) => {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
     res.write('Hello ');
-    res.write('World aaa!');
+    res.write('World. \nThis is /aa!');
     res.end();
   }
 }
 
 function pageNotFount(req: any, res: any) {
-  res.writeHead(404, { 'Content-type': 'text' });
+  res.writeHead(404, { 'Content-type': 'text/plain' });
   res.write('Page not found!');
   res.end();
 }
 
 const server = http.createServer(function (req, res) {
-  const [, reqPath] = /(.*)\?/.exec(req.url);
-  (router[reqPath] || pageNotFount)(req, res);
+  const url = decodeURIComponent(req.url);
+  const [, requestPath, requestQueryString = ''] = /([^\?]*)\??(.*)/.exec(url);
+  const requestQuery = requestQueryString === '' ? {} : requestQueryString.split('&').reduce((acc: { [key: string]: string }, queryParam) => {
+    const [key, value] = queryParam.split('=');
+    acc[key] = value;
+    return acc;
+  }, {});
+  (router[requestPath] || pageNotFount)(requestQuery, req, res);
 });
 
 server.listen(8080, function () {
